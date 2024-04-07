45783f9cb1902850e176873c1deebeb6aedcb052
iwan tri <iwantri257@gmail.com>
2024-04-07 22:30:22 +0700

    PWEB_PR

---
+++ Model/DataBase.php
@@ -0,0 +1,24 @@
+<?php
+
+Class Contact{ 
+
+    public $id_user;
+    public $password;
+    public $nomor_hp;
+    public $owner;
+    public $Alamat;
+    
+
+    static function select(){
+  
+    }
+    static function insert(){
+  
+    }
+    static function update(){
+  
+    }
+    static function delete(){
+  
+    }
+  }
